6cd958621fc85563b5c9b1efe7552de4e95ae104
ehnaton <ehnaton4@gmail.com>
2014-09-21 16:48:30 +0300

    threshold

--- signups/views.py
+++ signups/views.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from django.conf import global_settings
 from django.conf import settings
 from django.contrib import messages
@@ -41,31 +42,33 @@ def aboutus(request):
         context_instance=RequestContext(request))
 
 def nmp(request):
+
     import os
     import numpy as np
     from PIL import Image as img
     import matplotlib.pyplot as mp
+
     # return_dict = {'numpy_array': np.arange(10)}
     i=img.open(os.path.join(settings.IMAGES_ROOT, 'images', "numbers", "y0.3.png"))
-    iar=np.asarray(i)
+    iar=np.array(i)
     i2=img.open(os.path.join(settings.IMAGES_ROOT, 'images', "numbers", "y0.4.png"))
-    iar2=np.asarray(i2)
+    iar2=np.array(i2)
     i3=img.open(os.path.join(settings.IMAGES_ROOT, 'images', "numbers", "y0.5.png"))
-    iar3=np.asarray(i3)
+    iar3=np.array(i3)
     i4=img.open(os.path.join(settings.IMAGES_ROOT, 'images',  "sentdex.png"))
-    iar4=np.asarray(i4)
+    iar4=np.array(i4)
 
     def threshold(arr):
-        balance=[]
-        newarr=arr
+        balance = []
+        newarr = arr
         for row in arr:
             for pix in row:
-                avg=int(sum(pix[0:3])/3)
+                avg = reduce(lambda x, y: x+y, pix[:3])/len(pix[:3])
                 balance.append(avg)
-        bal=int(sum(balance)/len(balance))
+        bal = reduce(lambda x, y: x+y, balance)/len(balance)
         for r in newarr:
             for pixx in r:
-                if int(sum(pixx[0:3])/3)>=bal:
+                if reduce(lambda x, y: x+y, pixx[:3])/len(pixx[:3]) >= bal:
                     pixx[0]=255
                     pixx[1]=255
                     pixx[2]=255
@@ -76,8 +79,12 @@ def nmp(request):
                     pixx[2]=0
                     pixx[3]=255
         return newarr
-    a=iar
-    threshold(a)
+
+    threshold(iar)
+    threshold(iar2)
+    threshold(iar3)
+    threshold(iar4)
+
     fig=mp.figure()
     ax1=mp.subplot2grid((8,8),(0,0),rowspan= 4,colspan=4)
     ax2=mp.subplot2grid((8,8),(4,0),rowspan= 4,colspan=4)
